d9c601f7c4012b8319ca551767fd342187187eab
r--man <armanmtgh@gmail.com>
2016-09-25 10:50:17 -0700

    Styling upgrades

--- public/js/stocks.js
+++ public/js/stocks.js
@@ -1,5 +1,13 @@
-function stockStyle() {
-  var up_color = "#94ef1d";
-  var down_color = "#ef6b1d";
-  var neutral_color = "#1dd2ef";
+function stockStyle(prefix="",num,compare="flat",font_size="1em",symbol_height="12px") {
+  var up_
+  var down_
+  var flat_
+
+  var logo,color;
+
+  if(compare==="up") {logo = "images/up-logo.png"; color = "#5ae217";}
+  else if ( compare === "down") {logo = "images/down-logo.png"; color = "#ef6b1d";}
+  else if ( compare === "flat") {logo = "images/flat-logo.png"; color = "#1dd2ef";}
+
+  return `<div id=stock-wrapper style="font-size:${font_size}; color:${color}; height:${symbol_height};">${prefix + num}<img style="height:100%;" src="${logo}"" /></div>`
 }
